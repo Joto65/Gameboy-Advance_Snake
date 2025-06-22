4a2c66b363e844ded6f9709a0a2d48338addfd33
Joto65 <jotibeere@gmail.com>
2025-06-22 02:52:39 +0200

    meow

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,13 @@ fn main(gba: agb::Gba) -> ! {
 use agb::include_aseprite;
 use agb::display::object::Object;
 use agb::display::object::SpriteVram;
+use agb::fixnum::vec2;
+use agb::rng;
 
 include_aseprite!(
-    mod sprites,
-    "gfx/snake_sprites.aseprite"
-);
+            mod sprites,
+            "gfx/snake_sprites.aseprite"
+        );
 
 pub fn game1(mut gba: agb::Gba) -> !
 {
@@ -48,14 +50,14 @@ pub fn game1(mut gba: agb::Gba) -> !
 
     //snake starting values
     let mut head_position_x = 112;
-    let mut head_position_y = 72;
+    let mut head_position_y = 64;
     let mut body_positions_x = [0; 100];
     let mut body_positions_y = [0; 100];
     body_positions_x[0] = 112;
     body_positions_x[1] = 112;
-    body_positions_y[0] = 88;
-    body_positions_y[1] = 104;
-    let mut snake_length = 5;
+    body_positions_y[0] = 80;
+    body_positions_y[1] = 96;
+    let mut snake_length = 2;
 
     //starting movement
     let mut movement:[i32; 2] = [0, -16];
@@ -63,7 +65,7 @@ pub fn game1(mut gba: agb::Gba) -> !
 
     //snake objects
     let mut snake_head = Object::new(snake_head_v.clone());
-    let mut body = core::array::from_fn::<_, 100, _>(|_| Object::new(snake_body_straight_v.clone()));
+    let mut body = core::array::from_fn::<_, 119, _>(|_| Object::new(snake_body_straight_v.clone()));
 
     //set snake default position
     snake_head.set_pos((head_position_x, head_position_y));
@@ -77,8 +79,23 @@ pub fn game1(mut gba: agb::Gba) -> !
         StraightHorizontal,
         Turn
     }
-    let mut snake_body_type = core::array::from_fn::<_, 100, _>(|_| BodySprites::StraightVertical);
 
+
+    let mut snake_body_type = core::array::from_fn::<_, 119, _>(|_| BodySprites::StraightVertical);
+
+    let apple = SpriteVram::from(sprites::APPLE.sprite(0));
+    let banana = SpriteVram::from(sprites::BANANA.sprite(0));
+    let cherry = SpriteVram::from(sprites::CHERRY.sprite(0));
+    let kiwi = SpriteVram::from(sprites::KIWI.sprite(0));
+    let orange = SpriteVram::from(sprites::ORANGE.sprite(0));
+    let lemon = SpriteVram::from(sprites::LEMON.sprite(0));
+
+
+
+    let fruit_list = [apple, banana, cherry, kiwi, orange, lemon];
+
+    let mut fruit_amount = 0;
+    let mut fruits = core::array::from_fn::<_, 8, _>(|_| Object::new(fruit_list[0].clone()));
 
     let mut wait = 0;
 
@@ -128,95 +145,83 @@ pub fn game1(mut gba: agb::Gba) -> !
                 head_position_y = 0;
             }
 
-            
+
             for pos in (0..snake_length).rev() {
-                if pos == 0 {
+
+                if pos == 0 { //first body part
                     let new_sprite;
                     match snake_body_type[pos] {
-                        BodySprites::StraightVertical => { 
-                            new_sprite = snake_body_straight_v.clone(); 
-                            snake_body_type[pos +1] = BodySprites::StraightVertical; 
-                            body[pos].set_hflip(snake_head.hflip()); 
-                            body[pos].set_vflip(snake_head.vflip()); 
+                        BodySprites::StraightVertical => {
+                            new_sprite = snake_body_straight_v.clone();
+                            snake_body_type[pos +1] = BodySprites::StraightVertical;
+                            body[pos].set_hflip(snake_head.hflip());
+                            body[pos].set_vflip(snake_head.vflip());
                         },
-                        BodySprites::StraightHorizontal => { 
-                            new_sprite = snake_body_straight_h.clone(); 
-                            snake_body_type[pos +1] = BodySprites::StraightHorizontal; 
-                            body[pos].set_hflip(snake_head.hflip()); 
-                            body[pos].set_vflip(snake_head.vflip()); 
+                        BodySprites::StraightHorizontal => {
+                            new_sprite = snake_body_straight_h.clone();
+                            snake_body_type[pos +1] = BodySprites::StraightHorizontal;
+                            body[pos].set_hflip(snake_head.hflip());
+                            body[pos].set_vflip(snake_head.vflip());
                         },
-                        BodySprites::Turn => { 
-                            new_sprite = snake_body_turn.clone(); 
-                            snake_body_type[pos +1] = BodySprites::Turn; 
-                            body[pos].set_hflip(snake_head.hflip()); 
-                            body[pos].set_vflip(!snake_head.vflip()); 
+                        BodySprites::Turn => {
+                            new_sprite = snake_body_turn.clone();
+                            snake_body_type[pos +1] = BodySprites::Turn;
+                            body[pos].set_hflip(snake_head.hflip());
+                            body[pos].set_vflip(!snake_head.vflip());
                         }
                     };
                     body[pos].set_sprite(new_sprite);
                     body[pos].set_pos(snake_head.pos());
                 }
+
                 else if pos == snake_length -1 { //tail
-                    match snake_body_type[pos] {
-                        BodySprites::StraightVertical => { 
-                            body[pos].set_sprite(snake_body_end_v.clone()); 
-                            body[pos].set_vflip(body[pos -1].vflip()); 
-                            body[pos].set_hflip(!body[pos].hflip());
-                        },
-                        BodySprites::StraightHorizontal => { 
-                            body[pos].set_sprite(snake_body_end_h.clone()); 
-                            body[pos].set_hflip(body[pos -1].hflip()); 
-                            body[pos].set_vflip(!body[pos].vflip()); 
-                        },
-                        BodySprites::Turn => {
-                            for turn_pos in (0..pos).rev() {
-                                let is_even = (pos - turn_pos) % 2 == 0;
-                                match snake_body_type[turn_pos] {
-                                    BodySprites::Turn => (),
-                                    BodySprites::StraightVertical => {
-                                        if is_even {
-                                            body[pos].set_hflip(!body[turn_pos].hflip());
-                                            body[pos].set_vflip(!body[pos].vflip());
-                                            body[pos].set_sprite(snake_body_end_h.clone());
-                                        }
-                                        else {
-                                            body[pos].set_vflip(!body[turn_pos].vflip());
-                                            body[pos].set_hflip(!body[pos].hflip());
-                                            body[pos].set_sprite(snake_body_end_v.clone());
-                                        }
-                                        break;
-                                    },
-                                    BodySprites::StraightHorizontal => {
-                                        if is_even {
-                                            body[pos].set_vflip(body[turn_pos].vflip());
-                                            body[pos].set_hflip(body[pos].hflip());
-                                            body[pos].set_sprite(snake_body_end_v.clone());
-                                        }
-                                        else {
-                                            body[pos].set_hflip(body[turn_pos].hflip());
-                                            body[pos].set_vflip(body[pos].vflip());
-                                            body[pos].set_sprite(snake_body_end_h.clone());
-                                        }
-                                        break;
-                                    },
-                                }
-                            }
-                        } ,
-                    };
-                    body[pos].set_pos(body[pos -1].pos());
-                }
-                else {
                     let new_sprite;
                     match snake_body_type[pos] {
-                        BodySprites::StraightVertical => { 
-                            new_sprite = snake_body_straight_v.clone(); 
-                            snake_body_type[pos +1] = BodySprites::StraightVertical;  
+                        BodySprites::StraightVertical => {
+                            new_sprite = snake_body_end_v.clone();
+                            body[pos].set_hflip(!body[pos].hflip());
+                            body[pos].set_vflip(body[pos -1].vflip());
                         },
-                        BodySprites::StraightHorizontal => { 
-                            new_sprite = snake_body_straight_h.clone(); 
-                            snake_body_type[pos +1] = BodySprites::StraightHorizontal; 
+                        BodySprites::StraightHorizontal => {
+                            new_sprite = snake_body_end_h.clone();
+                            body[pos].set_hflip(body[pos -1].hflip());
+                            body[pos].set_vflip(!body[pos].vflip());
                         },
-                        BodySprites::Turn => { 
-                            new_sprite = snake_body_turn.clone(); 
+                        BodySprites::Turn => {
+                            if pos == 1 && body[pos -1].pos().x == snake_head.pos().x{
+                                new_sprite = snake_body_end_v.clone();
+                                body[pos].set_hflip(!body[pos].hflip());
+                                body[pos].set_vflip(!body[pos -1].vflip());
+                            }
+                            else if pos > 1 && body[pos -1].pos().x == body[pos -2].pos().x {
+                                new_sprite = snake_body_end_v.clone();
+                                body[pos].set_hflip(!body[pos].hflip());
+                                body[pos].set_vflip(!body[pos -1].vflip());
+                            }
+                            else {
+                                new_sprite = snake_body_end_h.clone();
+                                body[pos].set_hflip(body[pos -1].hflip());
+                                body[pos].set_vflip(!body[pos].vflip());
+                            }
+                        }
+                    };
+                    body[pos].set_sprite(new_sprite);
+                    body[pos].set_pos(body[pos -1].pos());
+                }
+
+                else { //other body parts
+                    let new_sprite;
+                    match snake_body_type[pos] {
+                        BodySprites::StraightVertical => {
+                            new_sprite = snake_body_straight_v.clone();
+                            snake_body_type[pos +1] = BodySprites::StraightVertical;
+                        },
+                        BodySprites::StraightHorizontal => {
+                            new_sprite = snake_body_straight_h.clone();
+                            snake_body_type[pos +1] = BodySprites::StraightHorizontal;
+                        },
+                        BodySprites::Turn => {
+                            new_sprite = snake_body_turn.clone();
                             snake_body_type[pos +1] = BodySprites::Turn;
                         }
                     };
@@ -226,10 +231,39 @@ pub fn game1(mut gba: agb::Gba) -> !
                     body[pos].set_vflip(body[pos -1].vflip());
 
                 }
+
             }
 
 
             snake_head.set_pos((head_position_x, head_position_y));
+
+            if fruit_amount == 0 {
+                fruit_amount = rng::next_i32().rem_euclid(8);
+                agb::println!("{fruit_amount}");
+                for fruit in 0..fruit_amount {
+                    fruits[fruit as usize].set_sprite(fruit_list[rng::next_i32().rem_euclid(5) as usize].clone());
+                    let mut search_free_pos = true;
+                    while search_free_pos {
+                        agb::println!("still searching");
+                        let xpos = rng::next_i32().rem_euclid(14) * 16;
+                        let ypos = rng::next_i32().rem_euclid(9) * 16;
+                        let mut free = true;
+                        if snake_head.pos() != vec2(xpos, ypos) {
+                            for pos in 0..snake_length {
+                                if body[pos].pos() == vec2(xpos, ypos) {
+                                    free = false;
+                                    break;
+                                };
+                            }
+                            if free == true {
+                                fruits[fruit as usize].set_pos(vec2(xpos, ypos));
+                                search_free_pos = false;
+                            }
+                        }
+                    }
+                }
+
+            }
             wait = 0;
         }
         else {
@@ -240,13 +274,19 @@ pub fn game1(mut gba: agb::Gba) -> !
         let mut frame = gfx.frame();
 
         snake_head.show(&mut frame);
-        body[0].show(&mut frame);
-        body[1].show(&mut frame);
-        body[2].show(&mut frame);
-        body[3].show(&mut frame);
-        body[4].show(&mut frame);
+        for pos in 0..snake_length {
+            body[pos].show(&mut frame);
+        }
+
+        for fruit in 0..fruit_amount {
+            fruits[fruit as usize].show(&mut frame);
+        }
+
+
 
         frame.commit();
 
+        //make rng harder to predict
+        let _ = rng::next_i32();
     }
 }
